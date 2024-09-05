5b75084da943df18ac82a2ecfaec7c8e69cc7fe6
Lorenzo Fanizzi <fanizzicontato@gmail.com>
2024-09-05 16:32:46 -0300

    refatoração do código

--- resolucao.js
+++ resolucao.js
@@ -2,12 +2,12 @@
     Nome: Lorenzo Fanizzi
     2º DSM */
 
-    let x = 5;          // No arquivo anterior, a variável "x" não estava sendo definida.
-    const y = 5;        // Foi definido "x" como let pois se trata de uma variável que houve uma reatribuição.
-    x = x + 10;         // A variável "y" foi definida como const para não haver reatribuição. 
-    
-    function somar() {  // Para a função "somar", foi utilizado return para evitar o uso de variáveis globais desnecessáriamente
-      return x + y 
-    }
-    
-    console.log(somar());
+function somar(a, b) {  // Para a função "somar", foi utilizado return para evitar o uso de variáveis globais desnecessáriamente
+    return a + b 
+}
+
+let x = 5;          // No arquivo anterior, a variável "x" não estava sendo definida.
+const y = 5;        // Foi definido "x" como let pois se trata de uma variável que houve uma reatribuição.
+x = x + 10;         // A variável "y" foi definida como const para não haver reatribuição. 
+
+console.log(somar(x, y));
